dd64b9e7089c0a3411a6d9c5a4fefadc14ddcdd6
Rasna <rasna@Ameen>
2025-11-22 10:48:13 +0530

    third commit

--- NewProject/src/main/java/day1/CollectionDemo.java
+++ NewProject/src/main/java/day1/CollectionDemo.java
@@ -3,73 +3,61 @@ package day1;
 import java.util.ArrayList;
 import java.util.Iterator;
 
-public class CollectionDemo 
-{
+public class CollectionDemo {
 
-	public static void main(String[] args) 
-	{
-		ArrayList ai=new ArrayList();
-		
-		//Adding
+	public static void main(String[] args) {
+		ArrayList ai = new ArrayList();
+
+		// Adding
 		ai.add(100);
 		ai.add("welcome");
 		ai.add('A');
-		
-		//size
+
+		// size
 		System.out.println(ai.size());
-		//Printing
-		
+		// Printing
+
 		System.out.println(ai);
-		
-		//insert
-		
+
+		// insert
+
 		ai.add(1, "java");
 		System.out.println(ai);
-		
-		//remove
+
+		// remove
 		ai.remove(1);
 		System.out.println(ai);
-		
-		//specific element
-		
+
+		// specific element
+
 		System.out.println(ai.get(1));
-		
-		//to read all the elements
-		
-		for(int i=0;i<2;i++)
-		{
+
+		// to read all the elements
+
+		for (int i = 0; i < 2; i++) {
 			System.out.println(i);
 		}
-		for(Object x:ai)
-		{
+		for (Object x : ai) {
 			System.out.println(x);
 		}
-		
-		Iterator it= ai.iterator();
+
+		Iterator it = ai.iterator();
 		{
-			while(it.hasNext())
-		
-			
+			while (it.hasNext())
+
 				System.out.println(it.next());
 		}
-		
-		//remove all()
-		
+
+		// remove all()
+
 		ai.add(100);
 		ai.add("welcome");
 		ai.removeAll(ai);
 		System.out.println(ai);
-		
-		//modify
-		
+
+		// modify
+
 		ai.set(2, "java");
-	
-		
-		
-		
-		
-		
-		
 
 	}
 
